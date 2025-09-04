edefb712a0963ad34c629afc9280d6bf30795c97
NoNama185 <lolcec185@gmail.com>
2025-09-04 15:14:00 +0500

    change delay and duration

--- script.js
+++ script.js
@@ -7,8 +7,8 @@
                 satellite.style.cssText = `
                     top: ${Math.random() * 100}%;
                     left: ${Math.random() * 100}%;
-                    animation-delay: ${Math.random() * -20}s;
-                    animation-duration: ${15 + Math.random() * 15}s;
+                    animation-delay: ${Math.random() * -70}s;
+                    animation-duration: ${15 + Math.random() * 55}s;
                 `;
                 animationContainer.appendChild(satellite);
             }
